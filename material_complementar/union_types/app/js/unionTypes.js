2619d5e2c9aa1d899cdb3b14aced7f8665716565
Leonardo Henrique <43254926+leonardolopesinf@users.noreply.github.com>
2019-04-28 16:55:37 -0300

    Criando pasta e union_types

---
+++ material_complementar/union_types/app/js/unionTypes.js
@@ -0,0 +1,5 @@
+var numeroOuLetra;
+numeroOuLetra = 2;
+console.log(numeroOuLetra);
+numeroOuLetra = 'Recebi uma string!';
+console.log(numeroOuLetra);
---
+++ material_complementar/union_types/app/ts/unionTypes.ts
@@ -0,0 +1,7 @@
+var numeroOuLetra: string | number;
+
+numeroOuLetra = 2;
+console.log(numeroOuLetra);
+
+numeroOuLetra = 'Recebi uma string!';
+console.log(numeroOuLetra);
